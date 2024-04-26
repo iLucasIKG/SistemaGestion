4b65c40623e014f7cdc7f1c5a40d74cf1ff4c2fd
lucas <lucas@gmail.com>
2024-04-25 21:38:59 -0300

    Los cambios del grupo 4

--- clientes/actualizar_clientes.php
+++ clientes/actualizar_clientes.php
@@ -3,6 +3,34 @@ include '../config/conexion.php';
 
     // Recuperamos los datos del formulario  
 
+    $id = $_POST["id"];
+    $apellido = $_POST["apellido"];
+    $nombre = $_POST["nombre"];
+    $dni = $_POST["dni"];
+    $sexo = $_POST["sexo"];
+    $domicilio = $_POST["domicilio"];
+    $telefono = $_POST["telefono"];
+    $correo = $_POST["correo"];
+    $datos = $_POST["datos"];
+    $nombre = $_POST["nombre"];
+   
+
+
     // Actualizamos los datos en la base de datos
+   
+    $sql = "UPDATE clientes SET nombre='$nombre',apellido='$apellido',dni=$dni,sexo='$sexo',domicilio='$domicilio',telefono='$telefono',correo='$correo',datos_importantes='$datos_importantes' WHERE id_cliente = $id";
+
+
+    //Ejecutamos la consulta y el resultado la almacenamos en la variable.
+    $resultado = mysqli_query($scon,$sql);
+
+    //preguntamos si los datos se guardaron correctamente
+    if ($resultado) {
+        echo "<script> alert('Se han actualizado los datos')</script>";
+        header("location:clientes.php");
+
+    }else {
+        echo "Ha ocurrido un error, intentalo de nuevo". mysqli_error($scon);
+    }
    
 ?>
